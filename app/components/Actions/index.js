086924cf4bf27fd09f672e66d7332e4a6eea83ed
pkrnacov <petra.krnacova@dhl.com>
2019-05-10 15:49:34 +0200

    Test

---
+++ app/components/Actions/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import messages from './messages';
+
+
+
+
+class Actions extends React.PureComponent {
+
+  render() {
+    return (
+		<div>Test</div>
+  }
+
+}
+
+export default Actions;
---
+++ app/components/Actions/messages.js
@@ -0,0 +1,12 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  helloTest1: {
+    id: 'app.components.Actions.helloTest1',
+    defaultMessage: 'Send Order as Email',
+  },
+  helloTest2: {
+    id: 'app.components.Action.helloTest2',
+    defaultMessage: 'Return Request',
+  },
+});
